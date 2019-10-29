41e97dd23d8c45812f4503e212f3a4c50d97fee7
kychery <55583936+kychery@users.noreply.github.com>
2019-10-29 19:46:07 +0900

    Update dz1n4.py

--- dz1n4.py
+++ dz1n4.py
@@ -1,7 +1,10 @@
 #задача4
 def is_prime(n):
-    d = 2
-    while n % d != 0:
-        d += 1
-    return d == n
+    if n>0:
+        d = 2
+        while n % d != 0:
+            d += 1
+        return d == n
+    else:
+        print('incorrect')
 is_prime(24)
