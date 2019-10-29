525063ab2af24a55a3db6d7ebb1f976653112a1a
kychery <55583936+kychery@users.noreply.github.com>
2019-10-29 19:55:35 +0900

    Update dz1n3.py

--- dz1n3.py
+++ dz1n3.py
@@ -1,6 +1,11 @@
 #задача3
 def is_power_of_two(n):
     from math import log
-    a = log(n, 2)
-    return a.is_integer()
-is_power_of_two(1024)
+    if n>0:
+        a = log(n, 2)
+        return a.is_integer()
+    else:
+        return False
+
+a = int(input())
+is_power_of_two(a)
