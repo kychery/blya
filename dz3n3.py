a1f010576be222afaa749c3bfb81821e405de023
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 01:33:10 +0900

    Create dz3n3.py

---
+++ dz3n3.py
@@ -0,0 +1,13 @@
+def text_generator(filename):
+    wile True:
+        with open(filename, "r") as f:
+            for line in f:
+                yield line/replace("\n", "")
+
+
+gen = text_generator("somefile.txt")
+for i, line in enumerate(gen):
+    print(line)
+    if i > 100:
+        break
+        
