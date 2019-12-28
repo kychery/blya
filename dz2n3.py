a0334f04a03af93ece75cd4a647e3a8a8744dd45
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 00:00:20 +0900

    Create dz2n3.py

---
+++ dz2n3.py
@@ -0,0 +1,20 @@
+def count_unique_codes(words):
+    morse_base = [
+        ".-", "-...", "-.-.", "-..", ".", "..-.", "--.", "....", "..", ".---",
+        "-.-", ".-..", "--", "-.", "---", ".--.", "--.-", ".-.", "...", "-",
+        "..-", "...-", ".--", "-..-", "-.--", "--.."]
+        
+    alph = [chr(l) for l in range(ord("a"), ord("z")+1)]
+    dict = {}
+    for i, j in zip(alph, morse_base):
+        dict[i] = j
+    same_words = set()
+    for word in words:
+        str = ""
+        for i in range(len(word)):
+            str = str + dict[word[i]]
+        same_words.add(str)
+    return same_words
+
+words = input("Введите слова: ").split()
+print(count_unique_codes(words))
