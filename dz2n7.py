13f91c8c9d1c7997529ec81e604e2e2b96c334a8
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 00:54:03 +0900

    Create dz2n7.py

---
+++ dz2n7.py
@@ -0,0 +1,11 @@
+def get_two_sum(nums, k):
+    for i in range(len(nums)):
+        for j in range(i + 1, len(nums)-1):
+            for l in range(j+1, len(nums)-2):
+                if int(nums[i]) + int(nums[j]) + int(nums[l]) == k:
+                    return [i, j, l]
+
+            
+nums = input("nums = ").split(', ')
+k = int(input("target = "))
+print(get_two_sum(nums, k))
