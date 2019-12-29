2d0e8471616e38d1c809782ab4b580e10bd9ab8a
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 12:03:29 +0900

    Update and rename dz1n3.py to 1_3.py

--- 1_3.py
+++ 1_3.py
@@ -1,11 +1,11 @@
-#задача3
 def is_power_of_two(n):
     from math import log
-    if n>0:
+    if n > 0:
         a = log(n, 2)
         return a.is_integer()
     else:
         return False
 
+    
 a = int(input())
 is_power_of_two(a)
