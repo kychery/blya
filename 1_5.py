67804e4217df67da6acb64b2798471fcd70d3d37
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 12:05:03 +0900

    Update and rename dz1n5.py to 1_5.py

--- 1_5.py
+++ 1_5.py
@@ -1,9 +1,10 @@
-#задача5
 def is_self_dividing(n):
     a = str(abs(n))
-    x=0
+    x = 0
     for i in range(len(a)):
-        if n % int(a[i]) !=0:
-            x=x+1
-    return x==0
+        if n % int(a[i]) != 0:
+            x = x + 1
+    return x == 0
+
+
 is_self_dividing(16)
