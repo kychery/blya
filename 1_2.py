0a6ba61e1313808e154e86bf8524b412dcce505f
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 12:03:03 +0900

    Update and rename dz1n2.py to 1_2.py

---
+++ 1_2.py
@@ -0,0 +1,8 @@
+def is_beauty(n):
+    a = str(abs(n))
+    A = 0
+    for i in range(len(a)):
+        A = A + int(a[i])
+    return n % A == 0
+
+is_beauty(145)
--- dz1n2.py
+++
@@ -1,9 +0,0 @@
-#задача2
-def is_beauty(n):
-    a=str(abs(n))
-    A=0
-    for i in range(len(a)):
-        A=A+int(a[i])
-    return n%A==0
-
-is_beauty(145)
