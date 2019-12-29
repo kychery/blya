0648fe6e2010a4d28486c3bf02011e6360ebac6c
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 12:04:22 +0900

    Update and rename dz1n4.py to 1_4.py

--- 1_4.py
+++ 1_4.py
@@ -1,10 +1,11 @@
-#задача4
 def is_prime(n):
-    if n>0:
+    if n > 0:
         d = 2
         while n % d != 0:
             d += 1
         return d == n
     else:
         return False
+    
+
 is_prime(24)
