cdc68ecd6f8adfc09cf75083c7764a7d8a1b1420
kychery <55583936+kychery@users.noreply.github.com>
2019-12-29 12:21:26 +0900

    Update 1_4.py

--- 1_4.py
+++ 1_4.py
@@ -1,5 +1,5 @@
 def is_prime(n):
-    if n > 0:
+    if n > 1:
         d = 2
         while n % d != 0:
             d += 1
@@ -8,4 +8,5 @@ def is_prime(n):
         return False
     
 
-is_prime(24)
+num = int(input())
+is_prime(num)
