dec7958ed2b9889a15ab06004e721c76092e9c17
kychery <55583936+kychery@users.noreply.github.com>
2020-01-18 12:22:03 +0800

    Update 3_1.py

--- 3_1.py
+++ 3_1.py
@@ -1,16 +1,21 @@
 def lru_cache(function):
-    cache = {}
+  cache = {}
 
-    def wrapper(*args, **kwargs):
-        args_key = args
-        kwargs_key = ('kwargs',)
-        kwargs_key = kwargs_key + tuple(kwargs.items())
-        key = args_key + kwargs_key
-        if key in cache:
-            return cache.get(args_key)
+  def wrapper(*args, **kwargs):
+    args_key = args
+    kwargs_key = ('kwargs',)
+    kwargs_key = kwargs_key + tuple(kwargs.items())
+    key = args_key + kwargs_key
+   
+    if key in cache:
+      return cache.get(key)
 
-        value = function(*args, **kwargs)
-        cache[args_key] = value
-        return value
+    result = function(*args, **kwargs)
+    cache[key] = result
+    return result
 
-    return wrapper
+  return wrapper
+
+@lru_cache
+def sum_two_numbers(a, b, c=3):
+  return a + b
