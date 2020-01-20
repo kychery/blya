67b4b48ccbb61394a1db2f45827902971915e0d7
kychery <55583936+kychery@users.noreply.github.com>
2020-01-20 16:01:52 +0900

    Update 2_7.py

--- 2_7.py
+++ 2_7.py
@@ -1,7 +1,7 @@
 def get_two_sum(nums, k):
     for i in range(len(nums)):
-        for j in range(i + 1, len(nums)-1):
-            for l in range(j+1, len(nums)-2):
+        for j in range(i + 1, len(nums)):
+            for l in range(j+1, len(nums)):
                 if int(nums[i]) + int(nums[j]) + int(nums[l]) == k:
                     return [i, j, l]
 
