6e492fcbd71cead7ba4d54ba81ed2f2398d8165d
kychery <55583936+kychery@users.noreply.github.com>
2020-01-20 17:36:41 +0900

    Update 1_3.py

--- 1_3.py
+++ 1_3.py
@@ -1,11 +1,23 @@
-def is_power_of_two(n):
-    from math import log
-    if n > 0:
-        a = log(n, 2)
-        return a.is_integer()
+def is_power_of_two(n, a=0, i=0):
+    if (n>=0) and (n<1):
+        a = 1
+        while a >= n:
+            a = 2**i
+            if n == a:
+                return True
+            else:
+                i -= 1
+        return False
+    elif n >= 1:
+        while a <= n:
+            a = 2**i
+            if n == a:
+                return True
+            else:
+                i += 1
+        return False
     else:
         return False
 
-    
-a = int(input())
-is_power_of_two(a)
+n = float(input())
+is_power_of_two(n)
